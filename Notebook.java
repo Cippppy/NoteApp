43688d745c466916da73acb89ddf59bd1fbb5358
Cippppy <98924381+Cippppy@users.noreply.github.com>
2023-01-19 21:39:54 -0500

    Added Notebook.java file
    Has getter and setter functions

---
+++ Notebook.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Notebook {
+
+    private ArrayList<String> notes;
+
+    public ArrayList<String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(ArrayList<String> newNotes) {
+        this.notes = newNotes;
+    }
+
+}
+
